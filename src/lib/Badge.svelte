5d8a4943e33b6d0f1158eaacd01727bdfe071635
nandotezza <fernando.tezza@gmail.com>
2022-05-23 11:13:57 -0300

    Create Badge component and Illustration component for the second modal

---
+++ src/lib/Badge.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let selectedRatingValue;
+</script>
+
+<div class="justify-self-center w-fit py-1.5 px-3 mb-1 rounded-3xl text-center text-sm text-primary bg-dark-blue-lighted">
+    You selected {selectedRatingValue} out of 5    
+</div>
